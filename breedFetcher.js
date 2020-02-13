21f40a9129b9bc6a21ef3764b442261c25f3ae30
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-13 22:40:07 +0000

    did some linting, made it look pretty

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,11 +1,4 @@
 const request = require('request');
-const fs = require('fs');
-const readline = require('readline');
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
 
 const catBreed = process.argv.slice(2);
 
@@ -14,7 +7,7 @@ request('https://api.thecatapi.com/v1/breeds/search?q=' + catBreed, (error, resp
   console.log('if there was an error, you will see it here: ', error);
   if (data == false) {
     console.log('That is not a valid cat breed, try again bro')
-  } else{
+  } else {
   // console.log(data);
   console.log('description: ', data[0]);
 }})
