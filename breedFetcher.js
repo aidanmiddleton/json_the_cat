8cffbedac1d602c4a1c4659c09eb202260513d8b
Aidan Middleton <aidanemiddleton@gmail.com>
2020-02-17 02:24:05 +0000

    linted

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,17 +2,16 @@ const request = require('request');
 
 
 const fetchBreedDescription = function(breedName, callback) {
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
-  const data = JSON.parse(body);
-  // console.log('type of body = ', (typeof body));
-  // console.log('data', (typeof data));
-  // console.log(error);
-  breed = data[0]
-  if (!breed) {
-    callback(error, null);
-  } else {
-  callback(error, breed.description);
-}})
+
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    const data = JSON.parse(body);
+    breed = data[0]
+    if (!breed) {
+      callback(error, null);
+    } else {
+      callback(error, breed.description);
+    }
+})
 
 };
 
